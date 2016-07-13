a2ee79577868aaa8e96814300239c171def7134f
CaryZheng <CaryZhengMy@gmail.com>
2016-07-13 17:55:48 +0800

    Add BlurDemo

---
+++ BlurDemo/BlurDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BlurDemo
+//
+//  Created by CaryZheng on 16/7/13.
+//  Copyright © 2016年 CaryZheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.redColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
